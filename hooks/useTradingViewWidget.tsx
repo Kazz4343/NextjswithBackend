76d29d10523f010d3eeaf26f4cfb3bc477c2aca5
Kritsada Rodrit <rodritkritsada@meta.com>
2025-10-01 18:13:12 +0700

    update my code

---
+++ hooks/useTradingViewWidget.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { useEffect, useRef } from 'react'
+
+const useTradingViewWidget = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default useTradingViewWidget;
